c76b6560bfb76e990023386a82cfdd1912a7418a
Sunday Oruwhone <sundayoruwhone@gmail.com>
2025-03-04 08:10:57 +0100

    Clean up - level 1.

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,18 +1,3 @@
-// import { Navigate } from "react-router-dom";
-// import { useSelector } from "react-redux";
-// import { RootState } from "../store";
-
-// const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-//   // const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-//   const isAuthenticated = useSelector(
-//     (state: RootState) => state.auth.isAuthenticated
-//   );
-
-//   return isAuthenticated ? children : <Navigate to="/" />;
-// };
-
-// export default ProtectedRoute;
-
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
